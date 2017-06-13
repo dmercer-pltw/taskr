a192b941c8a93a8c01d9d3c9196bcb5525f682b0
David Mercer <dmercer@fixedincomeacademy.com>
2017-06-13 11:13:33 -0400

    added flash msg and error handling

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+	def new
+		@task = Task.new
+	end
+	
+	def create
+		@task = Task.new(tasks_params)
+		if @task.save
+			flash[:notice] = "Task was successfully created"
+			redirect_to task_path(@task)
+		else
+			render 'new'
+		end
+	end
+	
+	def show
+		@task = Task.find(params[:id])
+	end
+	
+	
+	private
+		def tasks_params
+			params.require(:task).permit(:content)
+		end
+end
